462236fbf2f5212058d7130a2edc042a11eec97d
Lrdsnow <737201@salemkeizer.org>
2021-01-20 14:43:33 -0700

    orginize/delete

--- Mobile/JS/Games.js
+++
@@ -1,7 +0,0 @@
-var newwindow;
-function pop(url)
-{
-	newwindow=window.open(url,'name','height=400,width=200');
-	if (window.focus) {newwindow.focus()}
-}
-console.log('')
--- Mobile/JS/script.js
+++
@@ -1,4 +0,0 @@
-//WEBPAGE
-/*
-this is main mobile script
-*/
